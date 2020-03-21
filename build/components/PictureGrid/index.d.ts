3ffa46205c801636954bec985cf1792c77c2f8f9
Sazzad Sazib <sazib66@gmail.com>
2020-03-21 23:36:58 +0600

    feat: added css glamor

---
+++ build/components/PictureGrid/index.d.ts
@@ -0,0 +1,5 @@
+import * as React from 'react';
+interface Props {
+}
+declare const PictureGrid: React.FC<Props>;
+export default PictureGrid;
--- src/components/PictureGrid/index.tsx
+++ src/components/PictureGrid/index.tsx
@@ -1,9 +1,25 @@
-import * as React from 'react'
+import * as React from 'react';
+import { css } from 'glamor';
+
+let rule = css({
+  color: 'red',
+  ':hover': {
+    color: 'pink',
+  },
+  '@media(max-width: 800px)': {
+    color: 'green',
+    ':hover': {
+      color: 'yellow',
+    },
+  },
+});
 
 interface Props {}
 
 const PictureGrid: React.FC<Props> = () => (
-  <div role='heading'>React Picture Grid</div>
-)
+  <div role='heading' {...rule}>
+    React Picture Grid
+  </div>
+);
 
-export default PictureGrid
+export default PictureGrid;
